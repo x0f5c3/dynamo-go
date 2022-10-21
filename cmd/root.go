71a33c8cce4e7f889ac98662981d2b21f0a70b74
x0f5c3 <x0f5c3@tutanota.com>
2022-10-21 23:27:22 +0200

    First release

--- cmd/root.go
+++ cmd/root.go
@@ -10,14 +10,8 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "dynamo-go",
-	Short: "This cli template shows the date and time in the terminal",
-	Long: `This is a template CLI application, which can be used as a boilerplate for awesome CLI tools written in Go.
-This template prints the date or time to the terminal.`,
-	Example: `cli-template date
-cli-template date --format 20060102
-cli-template time
-cli-template time --live`,
+	Use:     "dynamo-go",
+	Short:   "Secure self-hosted dynamic DNS server",
 	Version: "v0.0.1", // <---VERSION---> Updating this version, will also create a new GitHub release.
 	// Uncomment the following lines if your bare application has an action associated with it:
 	// RunE: func(cmd *cobra.Command, args []string) error {
@@ -36,17 +30,23 @@ func Execute() {
 	go func() {
 		<-c
 		pterm.Warning.Println("user interrupt")
-		pcli.CheckForUpdates()
+		checkUpdate()
 		os.Exit(0)
 	}()
 
 	// Execute cobra
 	if err := rootCmd.Execute(); err != nil {
-		pcli.CheckForUpdates()
+		checkUpdate()
 		os.Exit(1)
 	}
+	checkUpdate()
+}
 
-	pcli.CheckForUpdates()
+func checkUpdate() {
+	err := pcli.CheckForUpdates()
+	if err != nil {
+		pterm.Fatal.Printfln("Failed to check for updates: %s", err)
+	}
 }
 
 func init() {
@@ -54,10 +54,13 @@ func init() {
 	// Fill the empty strings with the shorthand variant (if you like to have one).
 	rootCmd.PersistentFlags().BoolVarP(&pterm.PrintDebugMessages, "debug", "", false, "enable debug messages")
 	rootCmd.PersistentFlags().BoolVarP(&pterm.RawOutput, "raw", "", false, "print unstyled raw output (set it if output is written to a file)")
-	rootCmd.PersistentFlags().BoolVarP(&pcli.DisableUpdateChecking, "disable-update-checks", "", false, "disables update checks")
+	rootCmd.PersistentFlags().BoolVar(&pcli.DisableUpdateChecking, "no-update", false, "disables update checks")
 
 	// Use https://github.com/pterm/pcli to style the output of cobra.
-	pcli.SetRepo("x0f5c3/dynamo-go")
+	err := pcli.SetRepo("x0f5c3/dynamo-go")
+	if err != nil {
+		pterm.Fatal.Println(err)
+	}
 	pcli.SetRootCmd(rootCmd)
 	pcli.Setup()
 
